da33c87f6a7ee7f8573e1492784e331b4cef51b9
subjectdatadotcom <raja.bandla@subjectdata.com>
2025-04-15 18:19:38 -0500

    Add files via upload

---
+++ SGUM_UserAndGroupMappings.ps1
@@ -0,0 +1,28 @@
+﻿# Import the ShareGate module
+Import-Module Sharegate
+
+# Define the path to the CSV file containing user and group mappings
+$csvFile = "$MyDir\usermappings.csv"
+Write-Host $csvFile
+try {
+    # Import the CSV file into a table variable, specifying the delimiter as a comma
+    $table = Import-CSV $csvFile -Delimiter ","
+
+    # Create a new mapping settings object to store user and group mappings
+    $mappingSettings = New-MappingSettings
+
+    # Loop through each row in the imported CSV table
+    foreach ($row in $table) {
+        # Set user and group mappings for each row in the CSV file
+        $results = Set-UserAndGroupMapping -MappingSettings $mappingSettings -Source $row.SourceValue -Destination $row.DestinationValue
+    
+        # Output the source value for each row (for debugging or tracking purposes)
+        $row.SourceValue
+    }
+
+    # Export the user and group mappings to a file
+    Export-UserAndGroupMapping -MappingSettings $mappingSettings -Path "$MyDir\usermappings.sgum"
+}
+catch {
+    Write-Host "User mappings file already exists" -ForegroundColor Magenta
+}
